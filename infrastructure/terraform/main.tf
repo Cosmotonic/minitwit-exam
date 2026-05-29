3de30e02ef3841c53aacd794b6c8ae1eba352c72
Your Name <you@example.com>
2026-05-29 12:23:38 +0200

    lowering to 1gp server

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -10,7 +10,7 @@ resource "digitalocean_droplet" "minitwit-exam" {
   count    = 1 
   name     = "node-${count.index}" 
   image    = "ubuntu-24-04-x64"
-  size     = "s-1vcpu-2gb"
+  size     = "s-1vcpu-1gb"
   region   = "fra1"
   tags   = ["swarm"]
   ssh_keys = ["41:f4:ae:da:02:74:9e:2b:8c:a6:9f:54:67:d8:b8:f8"]
